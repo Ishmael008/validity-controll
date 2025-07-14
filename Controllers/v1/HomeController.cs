a5a6d0a7ba2018e53c684a80c3cf6a4623ad4840
Ishmael <ishmaels.vitorio@gmail.com>
2025-07-13 21:42:21 -0300

    Finalizando setup de produção

---
+++ Controllers/v1/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ValidityControl.Controllers.v1
+{
+    
+   
+        [ApiController]
+        [Route("/")]
+        public class HomeController : ControllerBase
+        {
+            [HttpGet]
+            public IActionResult Get() => Ok("API do ValidityControl está rodando!");
+        }
+
+    
+}
